83d2d1b08da9dde1e869d3f88a62720e2c99f5fb
dinnnnesh <dineshclg2@gmail.com>
2025-02-16 18:10:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,232 @@
+const books = [
+            {
+        "title": "Introduction to Python",
+        "author": "UDAYAN DAS",
+        "edition": "1st",
+        "category": "Python",
+        "url": "https://assets.openstax.org/oscms-prodcms/media/documents/Introduction_to_Python_Programming_-_WEB.pdf",
+        "image": "https://m.media-amazon.com/images/I/51JrTiMX+ML._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        "title": "Fundamentals of Python",
+        "author": "Kenneth A. Lambert",
+        "edition": "2nd",
+        "category": "Python",
+        "url": "https://ggnindia.dronacharya.info/Downloads/Sub-info/RelatedBook/PYTHON-TEXT-BOOK-2.pdf",
+        "image": "https://i.ytimg.com/vi/DNCAL1GqOvM/hq720.jpg"
+    },
+    {
+        "title": "Basics of Python",
+        "author": "Hans Fangohr",
+        "edition": "2nd",
+        "category": "Python",
+        "url": "https://www.freebookcentre.net/programming-books-download/gotoweb.php?id=11228",
+        "image": "https://dokumen.pub/img/200x200/an-introduction-to-python-for-computational-science-and-engineering.jpg"
+    },
+    {
+        "title": "Introduction to Programming Using Java",
+        "author": "David J. Eck",
+        "edition": "3rd",
+        "category": "Java",
+        "url": "https://www.iitk.ac.in/esc101/share/downloads/javanotes5.pdf",
+        "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSb4eoXBNHqsGm5N4Bip_u7AXiOujXOBji_of3ckZRu__bnUic9"
+    },
+    {
+        "title": "Cloud Computing",
+        "author": "Jim Dowling",
+        "edition": "1st",
+        "category": "Cloud Computing",
+        "url": "https://www.kth.se/social/files/554fa451f276544829be2e5e/9-cloud-computing.pdf",
+        "image": "https://cdn.slidesharecdn.com/ss_thumbnails/9-cloud-computing-230123134716-f0806a0d-thumbnail.jpg"
+    },
+    {
+        "title": "Data Structures & Algorithms in Python",
+        "author": "Mitchael T",
+        "edition": "none",
+        "category": "Python",
+        "url": "https://nibmehub.com/opac-service/pdf/read/Data%20Structures%20and%20Algorithms%20in%20Python.pdf",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1361147120i/13838796.jpg"
+    },
+            {
+        "title": "Learning Python",
+        "author": "Mark Lutz",
+        "edition": "4th",
+        "category": "Python",
+        "url": "https://cfm.ehu.es/ricardo/docs/python/Learning_Python.pdf",
+        "image": "https://m.media-amazon.com/images/I/91RcdlPx1CL._AC_UF1000,1000_QL80_.jpg"
+    },
+            {
+        "title": "Python Basics",
+        "author": "David Amos",
+        "edition": "4th",
+        "category": "Python",
+        "url": "https://static.realpython.com/python-basics-sample-chapters.pdf",
+        "image": "https://m.media-amazon.com/images/I/71Q0ZUIyUPL._UF1000,1000_QL80_.jpg"
+    },
+           {
+        "title": "Fluent Python",
+        "author": "Luciano Ramalho",
+        "edition": "2nd",
+        "category": "Python",
+        "url": "https://elmoukrie.com/wp-content/uploads/2022/05/luciano-ramalho-fluent-python_-clear-concise-and-effective-programming-oreilly-media-2022.pdf",
+        "image": "https://m.media-amazon.com/images/I/81OvszBEdhL._AC_UF1000,1000_QL80_.jpg"
+    },
+{
+        "title": "Python Programming",
+        "author": "Hans-Petter Halvorsen",
+        "edition": "none",
+        "category": "Python",
+        "url": "https://www.halvorsen.blog/documents/programming/python/resources/Python%20Programming.pdf",
+        "image": "https://www.halvorsen.blog/documents/programming/python/resources/images/TitlePage1.png"
+    },
+{
+        "title": "Java in a Nutshell",
+        "author": "Benjamin J. Evans & David Flanagan",
+        "edition": "6th",
+        "category": "Java",
+        "url": "https://www.r-5.org/files/books/computers/languages/java/main/Benjamin_Evans_David_Flanagan-Java_in_a_Nutshell_6th_ed-EN.pdf",
+        "image": "https://m.media-amazon.com/images/I/91R9esRljJL._AC_UF1000,1000_QL80_.jpg"
+    },
+{
+        "title": "Learn Java in 21 days",
+        "author": "Lauraa Lemay",
+        "edition": "none",
+        "category": "Java",
+        "url": "https://www.cs.cmu.edu/afs/cs.cmu.edu/user/gchen/www/download/java/LearnJava.pdf",
+        "image": "https://imgproxy2.pdfroom.com/UdhUjeLDXHy7jmVvrjyiJBrRxV3UW5tbf9XV50zMi0k/rs:auto:200:276:0/g:no/MHEySlFlSkxneEUucG5n.jpg"
+    },
+{
+        "title": "Java for beginners",
+        "author": "Riccardo Flask",
+        "edition": "2nd",
+        "category": "Java",
+        "url": "https://staff.um.edu.mt/__data/assets/pdf_file/0010/57169/jn.pdf",
+        "image": "https://2.imimg.com/data2/WE/TT/MY-/6-250x250.jpg"
+    },
+{
+        "title": "Advanced Java",
+        "author": "ANDRIY REDKO",
+        "edition": "none",
+        "category": "Java",
+        "url": "https://enos.itcollege.ee/~jpoial/allalaadimised/reading/Advanced-java.pdf",
+        "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1515427610i/37855594.jpg"
+    },
+{
+        "title": "Java Notes for Professionals",
+        "author": "Author",
+        "edition": "none",
+        "category": "Java",
+        "url": "https://goalkicker.com/JavaBook/JavaNotesForProfessionals.pdf",
+        "image": "https://goalkicker.com/JavaBook/JavaGrow.png"
+    },
+{
+        "title": "Java for Dummies",
+        "author": "Barry Burd",
+        "edition": "6th",
+        "category": "Java",
+        "url": "https://eprints.triatmamulya.ac.id/1702/1/Java%20for%20Dummies%20(6th%20ed.).pdf",
+        "image": "https://m.media-amazon.com/images/I/61hDCvdzBvL._AC_UF1000,1000_QL80_.jpg"
+    },
+{
+        "title": "Java with 101 Examples",
+        "author": "Atiwong Suchato",
+        "edition": "none",
+        "category": "Java",
+        "url": "https://www.cp.eng.chula.ac.th/books/wp-content/uploads/sites/5/2018/01/java101.pdf",
+        "image": "https://www.cp.eng.chula.ac.th/books/wp-content/uploads/sites/5/2018/01/java101_cover.png"
+    },
+{
+        "title": "JAVA Programming for Engineers",
+        "author": "JULIO SANCHEZ MARIA P. CANTON",
+        "edition": "none",
+        "category": "Java",
+        "url": "https://api.pageplace.de/preview/DT0400.9781420000009_A25118063/preview-9781420000009_A25118063.pdf",
+        "image": "https://m.media-amazon.com/images/I/61nQzwnRNJL._AC_UF1000,1000_QL80_.jpg"
+    },
+{
+        "title": "LEARN JAVATHE EASY WAY",
+        "author": "BRYSON PAYNE",
+        "edition": "none",
+        "category": "Java",
+        "url": "https://www.kea.nu/files/textbooks/humblelearn2code/learnjavatheeasyway.pdf",
+        "image": "https://m.media-amazon.com/images/I/81EGXCaqmwL._AC_UF1000,1000_QL80_.jpg"
+    },
+{
+        "title": "A History of Ancient",
+        "author": "Upinder Singh",
+        "edition": "noone",
+        "category": "History",
+        "url": "https://tehattagovtcollege.ac.in/Pdf/Resources/History/Online-learning/PDF-Book/INDIAN%20HISTORY%20ANCIENT%20TIME/A%20History%20of%20ancient%20an%20early%20mideaval%20India%20by%20Upinder%20sing.pdf",
+        "image": "https://m.media-amazon.com/images/I/91N9nb4kw-L._UF1000,1000_QL80_.jpg"
+    },
+{
+        "title": "THEMES IN INDIAN HISTORY",
+        "author": "Agrahayana",
+        "edition": "2nd",
+        "category": "History",
+        "url": "https://afeias.com/wp-content/uploads/2023/06/HISTORY-ENGLISH.pdf",
+        "image": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSbFmEBjNIGQpqPLwVbCuE8RvRSpiDgo3Lqg3C_20DK2g_RsQHW"
+    },
+{
+        "title": "The Penguin History of Early India",
+        "author": "ROMILA THAPAR",
+        "edition": "none",
+        "category": "History",
+        "url": "https://www.furkatingcollege.edu.in/wp-content/uploads/2021/05/History-of-Early-India-From-the-Origins-to-AD-1300_Thapar.pdf",
+        "image": "https://images-eu.ssl-images-amazon.com/images/I/41PTXOrQs7L._AC_UL600_SR600,600_.jpg"
+    },
+{
+        "title": "U.S. History",
+        "author": "Author",
+        "edition": "none",
+        "category": "History",
+        "url": "https://d3bxy9euw4e147.cloudfront.net/oscms-qa/media/documents/USHistory-LR.pdf",
+        "image": "https://imgv2-2-f.scribdassets.com/img/document/530323643/149x198/9b1a9227ed/1720295297?v=1"
+    },
+{
+        "title": "WORLD HISTORY",
+        "author": "Des Moines",
+        "edition": "none",
+        "category": "History",
+        "url": "https://publications.iowa.gov/27598/1/World%20History%201930.pdf",
+        "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR6s7lkJ38eFInm6Y4-YDr2xpRpbv1ZDpsqGgM9eN-_-UP7v9Qk"
+    },
+
+
+
+            
+        ];
+
+        function displayBooks(filteredBooks) {
+            const bookList = document.getElementById("bookList");
+            bookList.innerHTML = "";
+            filteredBooks.forEach(book => {
+                const bookItem = document.createElement("div");
+                bookItem.className = "bookItem";
+                bookItem.innerHTML = `<img src="${book.image}" class="bookImage">
+                                      <div class="bookTitle">${book.title}</div>
+                                      <div>Author: ${book.author}</div>
+                                      <div>Edition: ${book.edition}</div>
+                                      <div>Category: ${book.category}</div>
+                                      <a href="${book.url}" target="_blank" class="downloadBtn">Download</a>`;
+                bookList.appendChild(bookItem);
+            });
+        }
+
+        function searchBooks() {
+            const query = document.getElementById("searchBox").value.toLowerCase().split(" ");
+            const selectedCategory = document.getElementById("categorySelect").value;
+            const filteredBooks = books.filter(book => {
+                const bookData = `${book.title} ${book.author} ${book.edition} ${book.category}`.toLowerCase();
+                const matchesQuery = query.every(term => bookData.includes(term));
+                const matchesCategory = selectedCategory === "" || book.category === selectedCategory;
+                return matchesQuery && matchesCategory;
+            });
+            displayBooks(filteredBooks);
+        }
+
+        function shuffleBooks(array) {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+displayBooks(shuffleBooks(books));
